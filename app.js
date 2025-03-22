17821666826fbf7b505216f592a5d55e062407f5
unknown <mateox463@gmail.com>
2025-03-21 21:53:17 -0300

    se crea database

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const dbConnection = require("./router/database/config");
 const app = express();
 require("dotenv").config();
 
 app.use(express.json());
 
 app.use("/auth", require("./router/authRouter"));
+
+dbConnection();
 
 app.listen(process.env.PORT, () => {
   console.log("Server Levantado");
