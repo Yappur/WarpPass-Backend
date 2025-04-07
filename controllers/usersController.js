9dd31ef0f143b2bb03bbd2afb9a5a081ee3eecbf
unknown <mateox463@gmail.com>
2025-04-06 21:53:38 -0300

    Feature: se añade controlador ObtenerUsuarioPorId y ObtenerUsuarioPorToken

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,4 +6,37 @@ const obtenerUsuarios = async (req = request, res = response) => {
   res.status(200).json(obtenerUsuarios);
 };
 
-module.exports = { obtenerUsuarios };
+const obtenerUsuariosPorId = async (req = request, res = response) => {
+  try {
+    const obtenerUsuariosPorId = await Usuario.findById(req.params.id);
+
+    if (!obtenerUsuariosPorId) {
+      return res.status(400).json({ msg: "Usuario no encontrado con este ID" });
+    }
+
+    res.status(200).json(obtenerUsuariosPorId);
+  } catch (error) {
+    res.status(500).json({ msg: "Por favor contactarse con un administrador" });
+  }
+};
+
+const obtenerUsuarioPorToken = async (req = request, res = response) => {
+  try {
+    const usuarioId = req.usuario.id;
+
+    const usuario = await Usuario.findById(usuarioId);
+
+    if (!usuario) {
+      return res.status(400).json({ msg: "Usuario no encontrado con este ID" });
+    }
+    res.status(200).json(usuario);
+  } catch (error) {
+    res.status(500).json({ msg: "Por favor contactarse con un administrador" });
+  }
+};
+
+module.exports = {
+  obtenerUsuarios,
+  obtenerUsuariosPorId,
+  obtenerUsuarioPorToken,
+};
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const routerUsuarios = express.Router();
 
-const { obtenerUsuarios } = require("../controllers/usersController");
+const {
+  obtenerUsuarios,
+  obtenerUsuariosPorId,
+  obtenerUsuarioPorToken,
+} = require("../controllers/usersController");
+const verificarToken = require("../middlewares/verificarToken");
 
 routerUsuarios.get("/obtenerUsuarios", obtenerUsuarios);
+routerUsuarios.get("/obtenerUsuariosPorId/:id", obtenerUsuarios);
+routerUsuarios.get("/perfil", verificarToken, obtenerUsuarioPorToken);
 
 module.exports = routerUsuarios;
